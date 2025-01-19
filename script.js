6620339ccefb266cbd776d38d336309dfcb7ddbc
Riki <rmcal024@uottawa.ca>
2025-01-19 05:58:59 -0500

    wewewewewew

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.querySelector('.create-btn').addEventListener('click', () => {
-    alert('Create New button clicked!');
-  });
-  
-  document.querySelector('.learn-more-btn').addEventListener('click', () => {
-    alert('Learn More button clicked!');
-  });

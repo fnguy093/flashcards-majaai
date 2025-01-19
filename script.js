29c8213d4fa696fa9a9a1a0fa1ef622b2bc8dcb0
Felicia <fnguy093@uOttawa.ca>
2025-01-19 01:32:58 -0500

    Changes

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.querySelector('.create-btn').addEventListener('click', () => {
+    alert('Create New button clicked!');
+  });
+  
+  document.querySelector('.learn-more-btn').addEventListener('click', () => {
+    alert('Learn More button clicked!');
+  });
